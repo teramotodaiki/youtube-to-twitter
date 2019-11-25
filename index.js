90d8ffd29a5658549e0b3140524f4da2b8527490
teramotodaiki <i101323@gmail.com>
2019-11-25 15:15:03 +0900

    move to lib

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-const spawn = require('child_process').spawn;
-
-const ffmpeg = spawn(ffmpegPath, [
-  '-ss',
-  '0',
-  '-i',
-  './source.mp4',
-  '-t',
-  '5',
-  './output.mp4'
-]);
-ffmpeg.on('exit', () => {
-  console.log('complete!');
-});
+const trim = require('./lib/trim').trim;
