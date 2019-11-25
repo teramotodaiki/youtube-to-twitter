1ce0b536452d7125479abf12ba86cd4cd65531bf
teramotodaiki <i101323@gmail.com>
2019-11-25 17:41:28 +0900

    trim and tweet

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const trim = require('./lib/trim').trim;
+const tweet = require('./lib/tweet').tweet;
+
+trim('./source.mp4', './output.mp4', 0, 30)
+  .then(() => tweet('./output.mp4', '３０秒の動画のテストです'))
+  .catch(err => {
+    console.error(err);
+  });
