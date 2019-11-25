44f9efe9b01e3a2624e28a702f904eb61d537844
teramotodaiki <i101323@gmail.com>
2019-11-25 17:51:57 +0900

    use tmp path

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const trim = require('./lib/trim').trim;
 const tweet = require('./lib/tweet').tweet;
 
-trim('./source.mp4', './output.mp4', 0, 30)
-  .then(() => tweet('./output.mp4', '３０秒の動画のテストです'))
+trim('./source.mp4', 0, 30)
+  .then(output => tweet(output, '３０秒の動画のテストです'))
   .catch(err => {
     console.error(err);
   });
