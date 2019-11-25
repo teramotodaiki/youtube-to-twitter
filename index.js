279bb2b523c81404e9ae1c61813932572522f961
teramotodaiki <i101323@gmail.com>
2019-11-25 22:51:59 +0900

    download trim upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+const download = require('./lib/download').download;
 const trim = require('./lib/trim').trim;
 const tweet = require('./lib/tweet').tweet;
 
-trim('./source.mp4', 0, 30)
-  .then(output => tweet(output, '３０秒の動画のテストです'))
-  .catch(err => {
-    console.error(err);
-  });
+download('https://www.youtube.com/watch?v=9cY5m03rPpM')
+  .then(source => trim(source, 0, 30))
+  .then(output => tweet(output, 'ダウンロードしてアップロードするテストです'))
+  .catch(console.error);
